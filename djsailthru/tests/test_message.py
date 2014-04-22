b745cfd464362df5b3bfa113d0ed41e1e3bcad1b
Nick Lang <nick.lang@gmail.com>
2014-04-22 11:54:10 -0600

    adding another test

--- djsailthru/tests/test_message.py
+++ djsailthru/tests/test_message.py
@@ -1,5 +1,9 @@
+import json
+
 from djsailthru.mail.message import SailthruEmailMessage
 from djsailthru.tests.mock_backend import SailthruBackendMockTestCase
+
+from sailthru.sailthru_error import SailthruClientError
 
 
 class TestSailthruEamilMessage(SailthruBackendMockTestCase):
@@ -24,3 +28,11 @@ class TestSailthruEamilMessage(SailthruBackendMockTestCase):
         self.assertEqual(data['template'], 'Email Template')
         self.assertEqual(data['email'], 'foo@bar.com')
         self.assertEqual(data['vars'], {'vars': params})
+
+    def test_send_email_bad_email(self):
+        message = SailthruEmailMessage('foo@barcom', 'Email Template')
+        self.mock_api_post.return_value = self.MockResponse(
+            is_ok=False,
+            json=json.dumps({'errormsg': 'Invalid email: foo@barcom', 'error': 11}))
+        with self.assertRaises(SailthruClientError):
+            message.send()
