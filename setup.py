9c6136fc40c218a7dd97ff52030b5e654f7e1d08
Nick Lang <nick.lang@gmail.com>
2014-04-23 09:56:33 -0600

    adding requirements and updatingsetup

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 import os
+import re
 
 __doc__ = """
 App for using Sailthru as a django email backend.
 """
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            # TODO support version numbers
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
 
 
 def read(fname):
@@ -12,17 +28,14 @@ def read(fname):
 
 setup(
     name='djsailthru',
-    version="0.0.1",
+    version="0.0.2",
     description=__doc__,
     long_description=read('README.rst'),
     url="https://github.com/fxdgear/djsailthru",
     author="Nick Lang",
     author_email='nick@nicklang.com',
     packages=[package for package in find_packages() if package.startswith('djsailthru')],
-    install_requires=[
-        'Django>=1.4',
-        'sailthru-client>=2.1.3'
-    ],
+    install_requires=parse_requirements('requiremnts.txt'),
     zip_safe=False,
     include_package_data=True,
 )
